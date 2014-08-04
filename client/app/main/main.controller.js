53dcbdf080878999623347aeebd17aa8105709c4
meriadec <meriadec.pillet@gmail.com>
2014-08-04 17:27:33 +0200

    styling

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -18,10 +18,31 @@ angular.module('restockApp')
       return 'OK parsed';
     };
 
+    ruleparser.test= function (str) {
+    if (str === '5{name:s}') {
+      return {
+        valid: true
+      };
+    }
+      return {
+        valid: false,
+        msg: 'Invalid character'
+      };
+    };
+
     $scope.rule = { input: $routeParams.q || '' };
 
     $scope.$watch('rule.input', function () {
       $scope.rule.output = ruleparser.parse($scope.rule.input);
     });
+
+    $scope.isValid = function (str) {
+      var res = ruleparser.test(str);
+      if (res.valid === false) {
+        $scope.errorMsg = res.msg;
+        return false;
+      }
+      return true;
+    };
 
   });
