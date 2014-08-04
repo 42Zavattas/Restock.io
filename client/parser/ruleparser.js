0e160b8c36d0c0354fcd00fceed1b0d7b93e3e45
Aperçu <bgronon@gmail.com>
2014-08-04 17:23:28 +0200

    test(ruleparser): add a bunch of tests and begin of logic

--- client/parser/ruleparser.js
+++ client/parser/ruleparser.js
@@ -1,11 +1,31 @@
 (function(exports){
 
-  exports.test = function () {
-    return 'hello world'
+  function checkIsNull (val) {
+    return !!(val && val.length);
+  }
+
+  function limitLength (val) {
+    return !!(val.length <= 50);
+  }
+
+  var testFunctions = [
+    { f: checkIsNull, msg: 'Null given' },
+    { f:limitLength, msg: 'Too many characters' }
+  ];
+
+  exports.test = function (rule) {
+    for (i = 0; i < testFunctions.length; i++) {
+      if (testFunctions[i].f(rule) === false) {
+        return { valid: false, msg: testFunctions[i].msg };
+      }
+    }
+    return { valid: true };
   };
 
-  exports.convert = function (rule) {
-    
-  }
+  exports.parse = function (rule) {
+    if (!this.test(rule)) {
+      return null;
+    }
+  };
 
 })(typeof exports === 'undefined' ? this['ruleparser'] = {} : exports);
