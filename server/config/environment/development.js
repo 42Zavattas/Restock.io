0445e8ed571d367777a006f3bc1529731f5b15eb
Aperçu <bgronon@gmail.com>
2014-08-04 04:05:14 +0200

    Adding config files and link to MongoHQ

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/restock-dev'
+    uri: 'mongodb://zavatta:danylB@kahana.mongohq.com:10061/restock'
   },
 
   seedDB: true
