0d21db24217589c3de97483c59faf7fcdaeaba03
meriadec <meriadec.pillet@gmail.com>
2014-08-04 04:23:30 +0200

    compass & compass reset

--- Gruntfile.js
+++ Gruntfile.js
@@ -438,7 +438,7 @@ module.exports = function (grunt) {
             '<%= yeoman.client %>/app',
             '<%= yeoman.client %>/components'
           ],
-          compass: false
+          compass: true
         },
         files: {
           '.tmp/app/app.css' : '<%= yeoman.client %>/app/app.scss'
