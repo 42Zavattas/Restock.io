94a0a5477b1193961a185a2f678c66cf0bf1d41a
TheScaffolder <spam@forpurpose.io>
2014-08-04 03:47:28 +0200

    Scaffolding Restock.io app

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('restockApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
