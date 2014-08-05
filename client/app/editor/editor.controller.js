fffc6abc14e79716ec9370507d160af823bc2228
meriadec <meriadec.pillet@gmail.com>
2014-08-05 19:35:15 +0200

    add editor page and reset main

--- client/app/editor/editor.controller.js
+++ client/app/editor/editor.controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('restockApp')
-  .controller('EditorCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('EditorCtrl', function ($scope, $routeParams, $location) {
+
+    $scope.domain = $location.absUrl();
+    $scope.rule = { input: $routeParams.q || '', lexed: null };
+
+    $scope.isValid = function (str) {
+      var res = ruleparser.test(str), err = {};
+      if (res.valid === false) {
+        $scope.errorMsg = res.msg;
+        return false;
+      }
+      res = ruleparser.lex(str, err);
+      if (err.msg) {
+        $scope.errorMsg = err.msg;
+        return false;
+      }
+      $scope.errorMsg = '';
+      $scope.rule.lexed = res;
+      return true;
+    };
+
+    $scope.isValid($scope.rule.input);
+
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,8 @@
 'use strict';
 
 angular.module('restockApp')
-  .controller('MainCtrl', function ($scope, $routeParams, $location) {
+  .controller('MainCtrl', function () {
 
-    $scope.domain = $location.absUrl();
-    $scope.rule = { input: $routeParams.q || '', lexed: null };
-
-    $scope.isValid = function (str) {
-      var res = ruleparser.test(str), err = {};
-      if (res.valid === false) {
-        $scope.errorMsg = res.msg;
-        return false;
-      }
-      res = ruleparser.lex(str, err);
-      if (err.msg) {
-        $scope.errorMsg = err.msg;
-        return false;
-      }
-      $scope.errorMsg = '';
-      $scope.rule.lexed = res;
-      return true;
-    };
-
-    $scope.isValid($scope.rule.input);
+    console.log('jshint');
 
   });
