dec68d625d0603fa80f5dff7489c6d7047ad8ee4
Aperçu <bgronon@gmail.com>
2014-08-05 06:22:10 +0200

    style(signup): adding layout for better UI

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -2,28 +2,27 @@
 
 angular.module('restockApp')
   .controller('SignupCtrl', function ($scope, Auth, $location, $window) {
+
     $scope.user = {};
     $scope.errors = {};
 
     $scope.register = function(form) {
       $scope.submitted = true;
 
-      if(form.$valid) {
+      if (form.$valid) {
         Auth.createUser({
           name: $scope.user.name,
           email: $scope.user.email,
           password: $scope.user.password
         })
-        .then( function() {
-          // Account created, redirect to home
+        .then(function () {
           $location.path('/');
         })
-        .catch( function(err) {
+        .catch(function (err) {
           err = err.data;
           $scope.errors = {};
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
+          angular.forEach(err.errors, function (error, field) {
             form[field].$setValidity('mongoose', false);
             $scope.errors[field] = error.message;
           });
@@ -31,7 +30,7 @@ angular.module('restockApp')
       }
     };
 
-    $scope.loginOauth = function(provider) {
+    $scope.loginOauth = function (provider) {
       $window.location.href = '/auth/' + provider;
     };
   });
