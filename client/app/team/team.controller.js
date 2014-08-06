b52928a95f064a01fac119cde61b6c42b4304e24
Aperçu <bgronon@gmail.com>
2014-08-06 11:04:00 +0200

    feat(team): better config and randomizing

--- client/app/team/team.controller.js
+++ client/app/team/team.controller.js
@@ -2,5 +2,32 @@
 
 angular.module('restockApp')
   .controller('TeamCtrl', function ($scope) {
-    $scope.message = 'Hello';
+
+    $scope.members = [
+      {
+        name: 'Balthazar Gronon',
+        url: 'assets/images/b.jpg',
+        social: [
+          { name: 'github', url: 'https://github.com/Apercu' },
+          { name: 'stackoverflow', url: 'http://stackoverflow.com/users/2054072/aper%C3%A7u' },
+          { name: 'linkedin', url: 'https://www.linkedin.com/profile/view?id=333418914' }
+        ]
+      },
+      {
+        name: 'Mériadec Pillet',
+        url: 'assets/images/m.png',
+        social: [
+          { name: 'github', url: 'https://github.com/meriadec' },
+          { name: 'stackoverflow', url: 'http://stackoverflow.com/users/1845964/meriadec' },
+          { name: 'linkedin', url: 'https://www.linkedin.com/profile/view?id=334221265' }
+        ]
+      }
+    ];
+
+    var rand = Math.floor(Math.random() * $scope.members.length);
+
+    $scope.rMembers = [];
+    $scope.rMembers.push($scope.members[rand]);
+    $scope.rMembers.push($scope.members[rand === 0 ? 1 :0]);
+
   });
