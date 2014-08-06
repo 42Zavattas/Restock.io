0b763f5579cf0877fe78356bdfad75ea203187d8
meriadec <meriadec.pillet@gmail.com>
2014-08-06 02:39:00 +0200

    refactoring draw

--- client/app/editor/editor.controller.js
+++ client/app/editor/editor.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('restockApp')
-  .controller('EditorCtrl', function ($scope, $http, $routeParams, $location) {
+  .controller('EditorCtrl', function ($scope, $http, $routeParams, $location, $timeout) {
 
     $scope.domain = $location.protocol() + '://' + $location.host() + ':' + $location.port();
     $scope.rule = { input: $routeParams.q || '', lexed: null };
@@ -12,6 +12,58 @@ angular.module('restockApp')
       });
       console.log(rule);
     };
+
+    /**
+     * ------------------------------------------------------------------------
+     *                                 Draw
+     * ------------------------------------------------------------------------
+     */
+
+    var container = $('.rs-sketch-container'),
+        types = {
+          s: 'string',
+          n: 'number',
+          b: 'boolean',
+          d: 'date'
+        }
+
+    /**
+     * Returns if the node type is regular
+     */
+    function isRegular (type) {
+      return type.match(/^(s|n|b|d)$/);
+    }
+
+    /**
+     * Build the html for property/value couple
+     */
+    function buildProp (prop) {
+      return $(
+        '<div class="sk-couple">' +
+          '<span class="sk-prop">' + prop.name + ':</span>' +
+          (isRegular(prop.val.type) ? '<span class="sk-type">' + types[prop.val.type] + '</span>' : '') +
+        '</div>'
+      );
+    }
+
+    /**
+     * Draw a node and its children
+     */
+    function draw (lexed, deep) {
+      if (deep === 0) {
+        container.html('');
+      }
+      if (!lexed) {
+        return;
+      }
+      if (lexed.type === 'object') {
+        var prop;
+        for (var i = 0; i < lexed.props.length; i++) {
+          prop = buildProp(lexed.props[i]);
+          container.append(prop);
+        }
+      }
+    }
 
     $scope.isValid = function (str) {
       var res = ruleparser.test(str), err = {};
@@ -26,6 +78,7 @@ angular.module('restockApp')
       }
       $scope.errorMsg = '';
       $scope.rule.lexed = res;
+      draw(res, 0);
       return true;
     };
 
