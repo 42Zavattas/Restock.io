b8dbd82526e7b11923c0afd68247122d4a0e1943
Aperçu <bgronon@gmail.com>
2014-12-28 18:21:48 +0100

    chore(gen): remove useless task

--- Gruntfile.js
+++ Gruntfile.js
@@ -566,11 +566,6 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', function () {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve']);
-  });
-
   grunt.registerTask('test', function(target) {
     if (target === 'server') {
       return grunt.task.run([
