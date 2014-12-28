92697560d05797778da2bc6c8700534415318052
Aperçu <bgronon@gmail.com>
2014-12-28 18:49:33 +0100

    fix(deprecation): morgan

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,8 @@
 
 'use strict';
 
-var errors = require('./components/errors');
+var errors = require('./components/errors'),
+    path = require('path');
 
 module.exports = function(app) {
 
@@ -22,6 +23,6 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-      res.sendFile(app.get('appPath') + '/index.html');
+      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
     });
 };
